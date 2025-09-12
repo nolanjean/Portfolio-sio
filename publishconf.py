1bc1edd53b52b5fde2003de51195b1001153cfca
nolanjean <nobodyplayeur@gmail.com>
2025-09-12 16:48:33 +0200

    siteurl modif

--- publishconf.py
+++ publishconf.py
@@ -9,6 +9,6 @@ from pelicanconf import *
 
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://ljules.github.io/Portfolio-SIO"
+SITEURL = "https://github.com/nolanjean/Portfolio-sio"
 RELATIVE_URLS = False
 DELETE_OUTPUT_DIRECTORY = True
